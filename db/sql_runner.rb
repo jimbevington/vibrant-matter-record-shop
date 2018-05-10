975c215d27eb696a46f92a43c4828d6ceb77799c
Jim Bevington <jim.bevington@hotmail.com>
2018-05-10 22:05:37 +0100

    updated database credentials

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,8 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect( { dbname: 'record_shop', host: 'localhost' } )
+      db = PG.connect( {host: 'ec2-23-23-248-192.compute-1.amazonaws.com', dbname: 'dcgp30nliqvs6l', user: 'ppuzacpmccnkcs', password: '0c5d85f86ffe9156cf08c6e8212d8fac43733f4c42652dbf92851b9b3cb0c018', port: '5432'
+      } )
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
@@ -14,11 +15,3 @@ class SqlRunner
   end
 
 end
-
-# {
-#   dbname: 'dcgp30nliqvs6l',
-#   host: 'ec2-23-23-248-192.compute-1.amazonaws.com
-# ',
-#   port: '5432',
-#   password: '0c5d85f86ffe9156cf08c6e8212d8fac43733f4c42652dbf92851b9b3cb0c018',
-#   user: 'ppuzacpmccnkcs' }
